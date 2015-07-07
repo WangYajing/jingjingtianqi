5bbf20edd062f62d54280c9c82b3c636d223cd73
qianfeng <qianfeng@WYJ.local>
2015-07-07 20:38:06 +0800

    Initial Commit

---
+++ wang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wang
+//
+//  Created by qianfeng on 15/7/7.
+//  Copyright (c) 2015年 王亚静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wang
+//
+//  Created by qianfeng on 15/7/7.
+//  Copyright (c) 2015年 王亚静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
